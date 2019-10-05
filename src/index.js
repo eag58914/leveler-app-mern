da9eb92cd9f171006bc3dc9429a9b3f41e526d2a
Elco Garcia <garciaelco18@gmail.com>
2019-10-05 16:34:18 -0500

    added route component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './pages/App/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
 	<Router>
-		<App />
+		<Route component={App} />
 	</Router>,
 	document.getElementById('root')
 );
