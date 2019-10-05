e0481cc28976337fa33736de4736990a6e2cdd63
Elco Garcia <garciaelco18@gmail.com>
2019-10-05 16:35:30 -0500

    added functions

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import userService from '../../utils/userServices';
 
 class LoginPage extends Component {
 	state = {
@@ -7,32 +8,64 @@ class LoginPage extends Component {
 		pw: ''
 	};
 
-	handleSubmit = (e) => {
+	handleChange = (e) => {
+		this.setState({
+			// Using ES2015 Computed Property Names
+			[e.target.name]: e.target.value
+		});
+	};
+
+	handleSubmit = async (e) => {
 		e.preventDefault();
+		try {
+			console.log('hi1');
+			await userService.login(this.state);
+			console.log('hi2');
+			// Successfully signed up
+			this.props.handleSignupOrLogin();
+			console.log('hi3');
+			this.props.history.push('/');
+		} catch (err) {
+			// Invalid user data (probably duplicate email)
+			alert('Invalid Credentials!');
+		}
 	};
 
 	render() {
 		return (
-			<div>
+			<div className="LoginPage">
 				<header className="header-footer">Log In</header>
-				<form onSubmit={this.handleSubmit}>
-					<input
-						type="email"
-						className="form-control"
-						placeholder="Email"
-						value={this.state.email}
-						name="email"
-						onChange={this.handleChange}
-					/>
-					<input
-						type="password"
-						className="form-control"
-						placeholder="Password"
-						value={this.state.pw}
-						name="pw"
-						onChange={this.handleChange}
-					/>
-					<button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
+				<form className="form-horizontal" onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<div className="col-sm-12">
+							<input
+								type="email"
+								className="form-control"
+								placeholder="Email"
+								value={this.state.email}
+								name="email"
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="col-sm-12">
+							<input
+								type="password"
+								className="form-control"
+								placeholder="Password"
+								value={this.state.pw}
+								name="pw"
+								onChange={this.handleChange}
+							/>
+						</div>
+					</div>
+					<div className="form-group">
+						<div className="col-sm-12 text-center">
+							<button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
+							<Link to="/">Cancel</Link>
+						</div>
+					</div>
 				</form>
 			</div>
 		);
---
+++ src/pages/SignupPage/SignUpPage.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+class SignupPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { message: '' };
+	}
+
+	updateMessage = (msg) => {
+		this.setState({ message: msg });
+	};
+
+	render() {
+		return (
+			<div className="SignupPage">
+				<SignUpForm {...this.props} updateMessage={this.updateMessage} />
+				<p>{this.state.message}</p>
+			</div>
+		);
+	}
+}
+export default SignupPage;
