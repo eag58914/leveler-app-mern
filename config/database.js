ab1627cfa62e09cf0c90c94c91a66eda0d7702eb
Elco Garcia <garciaelco18@gmail.com>
2019-10-06 15:01:30 -0500

    connected mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/levler', { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
 db.once('connected', () => {
 	console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 });
+
+//'mongodb://localhost:27017/levler'
