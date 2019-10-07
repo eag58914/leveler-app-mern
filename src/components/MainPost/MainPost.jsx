3439a5896ee357f289017016d6941c156eca14d5
Elco Garcia <garciaelco18@gmail.com>
2019-10-07 18:44:05 -0500

    passed down delete function

--- src/components/MainPost/MainPost.jsx
+++ src/components/MainPost/MainPost.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MainPost = ({ content, user, id }) => {
+const MainPost = ({ content, user, id, handleDelete }) => {
 	return (
 		<li key={id} className="post">
 			<p>{content}</p>
 			<p>{user}</p>
+			<button onClick={() => handleDelete(id)}>Delete</button>
 		</li>
 	);
 };
@@ -15,5 +16,6 @@ export default MainPost;
 MainPost.propTypes = {
 	user: PropTypes.string,
 	content: PropTypes.string,
-	id: PropTypes.number
+	id: PropTypes.number,
+	handleDelete: PropTypes.func
 };
