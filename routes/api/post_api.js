91b85e37390674f693f6be022c08dc75d5e89d45
Elco Garcia <garciaelco18@gmail.com>
2019-10-07 12:44:05 -0500

    added route to controller

--- routes/api/post_api.js
+++ routes/api/post_api.js
@@ -3,6 +3,7 @@ var router = express.Router;
 var postCtrl = require('../../controllers/post');
 
 //routes for adding and showing posts/possbily renaming later
+router.get('/posts', postCtrl.getAllPosts);
 
 router.post('/create', postCtrl.createPost);
 
