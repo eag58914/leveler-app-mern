faa030e51e0c21a9c1beb04d96263df60283839e
Elco Garcia <garciaelco18@gmail.com>
2019-10-07 18:00:37 -0500

    added function to delete post

--- controllers/post.js
+++ controllers/post.js
@@ -2,7 +2,8 @@ const Post = require('../models/post');
 
 module.exports = {
 	createPost,
-	getAllPosts
+	getAllPosts,
+	deletePost
 };
 
 function createPost(req, res) {
@@ -16,3 +17,18 @@ function createPost(req, res) {
 function getAllPosts(req, res) {
 	Post.find({}).then((posts) => ({}.catch((error) => res.status(500).json(error.message))));
 }
+
+function deletePost(req, res) {
+	Post.findByIdAndRemove(req.params.id)
+		.then((blog) => {
+			res.status(200).json(blog);
+		})
+		.catch((error) => res.status(500).json(error.message));
+}
+
+//need to  figure  out eidt functionality
+// function edit(req,res){
+//     Post.findByIdAndUpdate(req.params.id).then(post=>{
+//         res.status(200).json(post)
+//     }).catch(error =>res.status(500).json(error.message))
+// }
