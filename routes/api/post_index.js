8643c87c9782d15e8e964fbbcb53a40208378de8
Elco Garcia <garciaelco18@gmail.com>
2019-10-07 11:56:24 -0500

    minor change

--- routes/api/post_index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router;
-var postCtrl = require('../../controllers/post');
-
-//routes for adding and showing posts/possbily renaming later
-router.post('/post', postCtrl.createPost);
-
-module.exports = router;
