b02e6ffacaabb4e7298fbc0d69de40cc3308f7c5
Elco Garcia <garciaelco18@gmail.com>
2019-10-08 15:53:40 -0500

    modified component Did Mount

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 import SignupPage from './pages/SignupPage/SignUpPage';
 import userService from './utils/userServices';
 import NavBar from '../src/components/NavBar/NavBar';
-import MainPost from './components/MainPost/MainPost';
 import PostForm from './components/PostForm/PostForm';
+
+import MainPostPage from './pages/MainPostPage/MainPostPage';
+
+//all post api imports
+import * as postAPI from './services/post-api';
 
 class App extends Component {
 	state = {
@@ -13,15 +17,13 @@ class App extends Component {
 		isShowing: true,
 		posts: []
 	};
-
-	componentDidMount = () => {
-		getAll().then((results) => {
-			console.log('started line 17:::', results); // this comes back as an arry of objects
-			this.setState({
-				posts: results // we do not need to spread it becuase we are just updating the posts to hold the arry of obj
-			});
+	/*--Lifecycle Methods for posting--*/
+	async componentDidMount() {
+		const posts = await postAPI.getAll(); // this comes back as an arry of objects
+		this.setState({
+			posts
 		});
-	};
+	}
 
 	handleShowForm = (event) => {
 		this.setState({
@@ -39,55 +41,52 @@ class App extends Component {
 	};
 
 	//these funciton handle all the CRUD for posts/ need to update to upload photos
-
-	handleAddPost = ({ author, post, votes }) => {
-		const url = 'http://localhost:3000/api/create';
-		const options = {
-			method: 'POST',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({ author, post, votes })
-		};
-		handleVerbs(url, options).then((results) => {
-			console.log(results);
-			this.setState({
-				posts: [ ...this.state.posts, results ]
-			});
-		});
+	handleAddPost = async ({ author, post, votes }) => {
+		const newPost = await postAPI.create(author, post, votes);
+		this.setState(
+			(state) => ({
+				posts: [ ...state.posts, newPost ]
+			}),
+			() => this.props.history.push('/')
+		);
 	};
 
-	handleDeletePost = (id) => {
-		const url = `http://localhost:3000/api/:${id}`;
-		const options = {
-			method: 'DELETE',
-			headers: {
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({ id })
-		};
-		handleVerbs(url, options).then((results) => {
-			console.log(results);
-			let newState = this.state.posts.filter((item) => this.state.posts[id] !== item);
-			this.setState({
-				posts: newState
-			});
-		});
-	};
+	// handleAddPost = ({ author, post, votes }) => {
+	// 	const url = 'http://localhost:3000/api/create';
+	// 	const options = {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'content-type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify({ author, post, votes })
+	// 	};
+	// 	handleVerbs(url, options).then((results) => {
+	// 		console.log(results);
+	// 		this.setState({
+	// 			posts: [ ...this.state.posts, results ]
+	// 		});
+	// 	});
+	// };
+
+	// handleDeletePost = (id) => {
+	// 	const url = `http://localhost:3000/api/:${id}`;
+	// 	const options = {
+	// 		method: 'DELETE',
+	// 		headers: {
+	// 			'content-type': 'application/json'
+	// 		},
+	// 		body: JSON.stringify({ id })
+	// 	};
+	// 	handleVerbs(url, options).then((results) => {
+	// 		console.log(results);
+	// 		let newState = this.state.posts.filter((item) => this.state.posts[id] !== item);
+	// 		this.setState({
+	// 			posts: newState
+	// 		});
+	// 	});
+	// };
 
 	render() {
-		const allPosts = this.state.posts.map((item, index) => {
-			return (
-				<MainPost
-					key={index}
-					user={item.author}
-					content={item.post}
-					id={index}
-					handleDelete={this.handleDeletePost}
-					votes={item.votes}
-				/>
-			);
-		});
 		return (
 			<div className="App">
 				<header className="App-header">
@@ -107,6 +106,7 @@ class App extends Component {
 							<SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />
 						)}
 					/>
+					<Route exact path="/" render={({ history }) => <MainPostPage posts={this.state.posts} />} />>
 				</Switch>
 
 				{this.state.user ? (
@@ -114,7 +114,6 @@ class App extends Component {
 				) : (
 					<button onClick={this.handleShowForm}>Add Post</button>
 				)}
-				<ul>{allPosts}</ul>
 			</div>
 		);
 	}
@@ -123,15 +122,15 @@ class App extends Component {
 export default App;
 
 //get all post,TODO: set up routes and controller to get all the post
-async function getAll() {
-	const url = '/api/posts';
-	const initialFetch = await fetch(url);
-	const fetchJSON = await initialFetch.json();
-	return await fetchJSON;
-}
+// async function getAll() {
+// 	const url = '/api/posts';
+// 	const initialFetch = await fetch(url);
+// 	const fetchJSON = await initialFetch.json();
+// 	return await fetchJSON;
+// }
 
-async function handleVerbs(url, options) {
-	const initalFetch = await fetch(url, options);
-	const fetchJSON = await initalFetch.json();
-	return await fetchJSON;
-}
+// async function handleVerbs(url, options) {
+// 	const initalFetch = await fetch(url, options);
+// 	const fetchJSON = await initalFetch.json();
+// 	return await fetchJSON;
+// }
