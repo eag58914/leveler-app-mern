54616cc140d7a3d38621ab677096ecfc32c365f3
Elco Garcia <garciaelco18@gmail.com>
2019-10-08 15:53:51 -0500

    added main post page

---
+++ src/pages/MainPostPage/MainPostPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MainPost from '../../components/MainPost/MainPost';
+
+function PostPageList(props) {
+	return (
+		<div>
+			<h1>My Posts</h1>
+			<div>
+				{props.posts.map((item, index) => (
+					<MainPost key={index} user={item.author} content={item.post} id={index} votes={item.votes} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+export default PostPageList;
