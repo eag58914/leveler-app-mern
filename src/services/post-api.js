3cc0776d62b090d849f3f77aa11e2fc88dc729c4
Elco Garcia <garciaelco18@gmail.com>
2019-10-08 16:48:45 -0500

    fixed bug

--- src/services/post-api.js
+++ src/services/post-api.js
@@ -4,10 +4,10 @@ export function getAll() {
 	return fetch(BASE_URL).then((res) => res.json());
 }
 
-export function create({ author, post, votes }) {
+export function create(post) {
 	return fetch(BASE_URL, {
 		method: 'POST',
 		headers: { 'content-type': 'application/json' },
-		body: JSON.stringify({ author, post, votes })
-	});
+		body: JSON.stringify(post)
+	}).then((res) => res.json());
 }
