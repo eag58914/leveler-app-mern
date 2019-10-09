b4a8228e9c89f22627ffd129f35c0a6b7f31f65a
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 10:39:46 -0500

    made small changes

--- src/pages/MainPostPage/MainPostPage.jsx
+++ src/pages/MainPostPage/MainPostPage.jsx
@@ -4,7 +4,7 @@ import MainPost from '../../components/MainPost/MainPost';
 function PostPageList(props) {
 	return (
 		<div>
-			<h1>My Posts</h1>
+			<h1>All Posts</h1>
 			<div>
 				{props.posts.map((item, index) => (
 					<MainPost
@@ -13,6 +13,7 @@ function PostPageList(props) {
 						content={item.post}
 						id={index}
 						votes={item.votes}
+						comments={item.comments}
 						handleDeletePost={props.handleDeletePost}
 					/>
 				))}
