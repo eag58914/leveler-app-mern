17188604c5638c94444b8eaf352d1c4de7a0186c
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 10:39:35 -0500

    added comments to post

--- src/components/MainPost/MainPost.jsx
+++ src/components/MainPost/MainPost.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MainPost = ({ content, user, id, handleDeletePost, votes }) => {
+const MainPost = ({ content, user, id, handleDeletePost, votes, comments }) => {
 	console.log(votes, content, user, handleDeletePost);
 	return (
 		<li key={id}>
-			<p>{content}</p>
 			<p>{user}</p>
+			<p>{content}</p>
+			<button>upvote</button>
 			<p>{votes}</p>
+			<button>downvote</button>
+			<h6>Comments</h6>
+			<p>{comments}test</p>
 			{user ? <button onClick={() => handleDeletePost(id)}>Delete</button> : null}
 		</li>
 	);
@@ -20,5 +24,6 @@ MainPost.propTypes = {
 	content: PropTypes.string,
 	id: PropTypes.number,
 	handleDelete: PropTypes.func,
-	votes: PropTypes.number
+	votes: PropTypes.number,
+	comments: PropTypes.string
 };
