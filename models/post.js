237f7bcf9c17f0c804fb24834ca93cee9753eebf
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 10:39:07 -0500

    added comment schema to post schema

--- models/post.js
+++ models/post.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var commentSchema = new Schema(
+	{ content: String },
+	{
+		timestamps: true
+	}
+);
+
 var PostSchema = new Schema({
 	author: { type: String },
 	post: { type: String },
-	votes: { type: Number }
+	votes: { type: Number },
+	comments: [ commentSchema ]
 });
 
 module.exports = mongoose.model('Blog', PostSchema);
