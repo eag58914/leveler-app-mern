9816fdef7ca0979829e5a44792816d73dd53a0af
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 15:04:35 -0500

    add link to add post

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 const NavBar = (props) => {
 	let nav = props.user ? (
 		<div>
+			<span className="NavBar-welcome">WELCOME, {props.user.name}</span>
+			&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+			<Link to="/add_post" className="NavBar-Link">
+				Add a Post
+			</Link>
+			&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
 			<Link to="" className="NavBar-link" onClick={props.handleLogout}>
 				LOG OUT
 			</Link>
-			&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-			<span className="NavBar-welcome">WELCOME, {props.user.name}</span>
 		</div>
 	) : (
 		<div>
