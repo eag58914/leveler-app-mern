f5fb6fa547662fe0fb044e2cbfd7ee2f39b52ddb
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 15:04:44 -0500

    added comment page

---
+++ src/pages/AddCommentPage/AddCommentPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import commentForm from '../../components/CommentForm/CommentForm';
+const AddCommentPage = () => {
+	return <commentForm />;
+};
+
+export default AddCommentPage;
