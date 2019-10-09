86aad09c0b61ea4340907c70f5fd250e19d6647f
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 15:05:01 -0500

    fixed bug

--- src/services/post-api.js
+++ src/services/post-api.js
@@ -14,7 +14,7 @@ export function create(post) {
 
 //delete function is not grabbing the ID talk to Chris
 export function deleteOne(id) {
-	return fetch(`${BASE_URL}/${id})`, {
+	return fetch(`${BASE_URL}/${id}`, {
 		method: 'DELETE'
 	}).then((res) => res.json());
 }
