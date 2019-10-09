e11300d36718e512dd1232dab0b7c07a8fca2c2d
Elco Garcia <garciaelco18@gmail.com>
2019-10-09 09:37:38 -0500

    passed down delete post

--- src/components/MainPost/MainPost.jsx
+++ src/components/MainPost/MainPost.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MainPost = ({ content, user, id, handleDelete, votes }) => {
-	console.log(votes, content, user, handleDelete);
+const MainPost = ({ content, user, id, handleDeletePost, votes }) => {
+	console.log(votes, content, user, handleDeletePost);
 	return (
 		<li key={id}>
 			<p>{content}</p>
 			<p>{user}</p>
 			<p>{votes}</p>
-			{user ? <button onClick={() => handleDelete(id)}>Delete</button> : null}
+			{user ? <button onClick={() => handleDeletePost(id)}>Delete</button> : null}
 		</li>
 	);
 };
