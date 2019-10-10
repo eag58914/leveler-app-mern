3920d56b698d0cd437973b46bb68c322101b56b5
Elco Garcia <garciaelco18@gmail.com>
2019-10-10 14:52:13 -0500

    created post route for adding comments

--- routes/api/post_api.js
+++ routes/api/post_api.js
@@ -7,5 +7,6 @@ router.get('/', postCtrl.getAllPosts);
 router.post('/', postCtrl.createPost);
 router.get('/:id', postCtrl.showPost);
 router.delete('/:id', postCtrl.deletePost);
+router.post('/:id', postCtrl.addComment);
 
 module.exports = router;
