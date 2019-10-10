c25eee2b5ec3cbc3ff2113cd5679b3855ef7f040
Elco Garcia <garciaelco18@gmail.com>
2019-10-10 16:39:36 -0500

    passed down handleadd comment function

--- src/pages/AddCommentPage/AddCommentPage.jsx
+++ src/pages/AddCommentPage/AddCommentPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CommentForm from '../../components/CommentForm/CommentForm';
 const AddCommentPage = (props) => {
-	return <CommentForm handleAddComment={props.handleAddComment} />;
+	return <CommentForm handleAddComment={props.handleAddComment} id={props.id} />;
 };
 
 export default AddCommentPage;
