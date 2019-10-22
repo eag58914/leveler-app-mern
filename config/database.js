0f110146cc5d942d996f45f6ae7f4f2083e9d0b0
Elco Garcia <garciaelco18@gmail.com>
2019-10-22 16:27:00 -0500

    Added lone to avoid deprecation

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://leveler:Albatross1894@cluster0-jli2d.mongodb.net/myapp?retryWrites=true&w=majority', {
-	useNewUrlParser: true
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
