b4bb2cef77af438f7bae6684da3c39a05ebf7faa
Elco Garcia <garciaelco18@gmail.com>
2019-11-10 12:22:14 -0600

    added path for chat _room api

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cors = require('cors');
+const socketio = require('socket.io');
+const http = require('http');
 
 require('dotenv').config();
 require('./config/database');
@@ -10,6 +12,7 @@ const app = express();
 
 var apiRouter = require('./routes/api/post_api');
 var userRouter = require('./routes/api/users');
+var chatRoomRouter = require('./routes/api/chat_api');
 // var imageRouter = require('./routes/api/images_uploader');
 
 app.use(cors());
@@ -20,8 +23,9 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-//api route, hopefully works
+//api routes
 app.use('/api', apiRouter);
+app.use('/join', chatRoomRouter);
 
 //app.use('/api/images', imageRouter);
 
@@ -32,9 +36,25 @@ app.get('/*', function(req, res) {
 });
 
 const port = process.env.PORT || 3001;
+const socketPort = process.env.PORT || 5000;
 
 app.listen(port, function() {
 	console.log(`Express app running on port ${port}`);
 });
 
-console.log('listening on port ', port);
+//socket connection
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+	console.log('We have a new connection!');
+
+	socket.on('disconnect', () => {
+		console.log('User has left :(');
+	});
+});
+
+server.listen(socketPort, () =>
+	console.log(`socket.io app running on port ${port}
+	`)
+);
