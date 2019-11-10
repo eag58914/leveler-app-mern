e186dc75475622b671fd3aacbe16a3c0735aab59
Elco Garcia <garciaelco18@gmail.com>
2019-11-10 17:29:19 -0600

    implemented sockets to send message, broadcast

--- server.js
+++ server.js
@@ -47,8 +47,9 @@ app.listen(port, function() {
 	console.log(`Express app running on port ${port}`);
 });
 
-//socket connection
+//socket connection and functions
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require(`./controllers/chat_room_user.js`);
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -57,7 +58,24 @@ server.listen(socketPort, () => {
 });
 
 io.on('connection', (socket) => {
-	console.log('We have a new connection!');
+	socket.on('join', ({ name, room }, callback) => {
+		const { error, user } = addUser({ id: socket.id, name, room });
+
+		if (error) return callback(error);
+
+		socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+		socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+		socket.join(user.room);
+
+		callback();
+	});
+
+	socket.on('sendMessage', (message, callback) => {
+		const user = getUser(socket.id);
+		io.to(user.room).emit('message', { user: user.name, text: message });
+
+		callback();
+	});
 
 	socket.on('disconnect', () => {
 		console.log('User has left :(');
