ef23c1d2bf8db5347d759aee9b3a492d777b87f9
Elco Garcia <garciaelco18@gmail.com>
2019-11-10 22:52:49 -0600

    message component made

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import Message from '../Message/Message';
+import('./Messages.css');
+
+const Messages = ({ messages, name }) => {
+	return (
+		<ScrollToBottom>
+			{messages.map((message, i) => (
+				<div key={i}>
+					<Message message={message} names={name} />
+				</div>
+			))}
+		</ScrollToBottom>
+	);
+};
+
+export default Messages;
