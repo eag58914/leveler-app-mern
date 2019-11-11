4a2e2bf57a685332700dc331fcf19a5b57757908
Elco Garcia <garciaelco18@gmail.com>
2019-11-11 17:06:38 -0600

    check message and user

--- server.js
+++ server.js
@@ -75,6 +75,8 @@ io.on('connect', (socket) => {
 
 	socket.on('sendMessage', (message, callback) => {
 		const user = getUser(socket.id);
+		console.log(message);
+		console.log(user);
 
 		io.to(user.room).emit('message', { user: user.name, text: message });
 
