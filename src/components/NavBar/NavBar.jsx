f0b1f2019470613e8947320e59d30529ba4f3805
Elco Garcia <garciaelco18@gmail.com>
2020-01-06 14:44:13 -0600

    changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import('./NavBar.css');
+import '/Users/elcogarcia/levler_version_2/src/components/NavBar/NavBar.css';
 
 const NavBar = (props) => {
 	let nav = props.user ? (
