948af5cef25a1177d03d8d98937587c9f1af6a78
Elco Garcia <garciaelco18@gmail.com>
2020-01-16 10:47:15 -0600

    reworking user being passed

--- src/components/MainPost/MainPost.jsx
+++ src/components/MainPost/MainPost.jsx
@@ -4,37 +4,37 @@ import styles from './MainPost.module.css';
 
 import { Link } from 'react-router-dom';
 
-const MainPost = ({ content, user, id, handleDeletePost, votes, comments, client, handleId }) => {
+const MainPost = ({ content, id, handleDeletePost, votes, comments, client, handleId }) => {
 	const allComments = comments.map((comment) => {
 		return <p className={styles.Comments}>{comment.content}</p>;
 	});
 	return (
-		<div className="card col-sm-8" align="center">
-			<div className="card-body " align="center">
-				<li className={styles.MainPost}>
-					{/* <p>{client}</p> */}
-					<h4>Title:</h4>
-					<h3>Post:</h3>
-					<p className={styles.Content}>{content}</p>
-					<div className={styles.ButtonLayout}>
-						<input className={styles.UpVote} type="image" src="images/upvote-icon-17.jpg" alt="" />
-						<p>{votes}</p>
-						<input className={styles.DownVote} type="image" src="images/upvote-icon-17.jpg" alt="" />
+		<div>
+			<li className={styles.MainPost}>
+				<h4>Title:</h4>
+				<h3>Post:</h3>
+				<p className={styles.Content}>{content}</p>
+				<div className={styles.ButtonLayout}>
+					<input className={styles.UpVote} type="image" src="images/upvote-icon-17.jpg" alt="" />
+					<p>{votes}</p>
+					<input className={styles.DownVote} type="image" src="images/upvote-icon-17.jpg" alt="" />
+					{client ? (
 						<Link to="/add_comment">
 							<button className="btn btn-outline-primary" onClick={() => handleId(id)}>
 								add comment
 							</button>
 						</Link>
-						{client ? (
-							<button onClick={() => handleDeletePost(id)} class="btn btn-outline-danger">
-								X
-							</button>
-						) : null}
-					</div>
-					<h6>Comments:</h6>
-					<div>{allComments}</div>
-				</li>
-			</div>
+					) : null}
+
+					{client ? (
+						<button onClick={() => handleDeletePost(id)} class="btn btn-outline-danger">
+							X
+						</button>
+					) : null}
+				</div>
+				<h6>Comments:</h6>
+				<div>{allComments}</div>
+			</li>
 		</div>
 	);
 };
