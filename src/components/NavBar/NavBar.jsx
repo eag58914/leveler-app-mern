7057fab87aac17ad36c7b66e5b0ba75a4bd1db3a
Elco Garcia <garciaelco18@gmail.com>
2020-01-16 10:47:57 -0600

    navbar css changed to modules

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../NavBar/NavBar.css';
+import style from './NavBar.module.css';
 
 const NavBar = (props) => {
 	let nav = props.user ? (
-		<div className="nav-bar">
-			<Link to="/" className="main-icon">
+		<div className={style.navBar}>
+			<Link to="/" className={style.mainIcon}>
 				<h1>Leveler</h1>
 			</Link>
 
-			<Link to="/add_post" className="NavBar-Link">
+			<Link to="/add_post" className={style.NavBarLink}>
 				Add a Post
 			</Link>
 
@@ -17,26 +17,26 @@ const NavBar = (props) => {
 				Join a Chat
 			</Link> */}
 
-			<input className="form-control" type="text" placeholder="Search" aria-label="Search" />
-			<span className="NavBar-welcome">{props.user.name}</span>
+			<input className={style.formControl} type="text" placeholder="Search" aria-label="Search" />
+			<span className={style.NavBarWelcome}>{props.user.name}</span>
 			<Link to="" className="NavBar-Link" onClick={props.handleLogout}>
 				LOG OUT
 			</Link>
 		</div>
 	) : (
-		<div>
-			<Link to="/login" className="NavBar-Link">
+		<div className={style.logoutDefault}>
+			<Link to="/login" className={style.NavBarLink}>
 				LOG IN
 			</Link>
 			&nbsp;&nbsp;|&nbsp;&nbsp;
-			<Link to="/signup" className="NavBar-Link">
+			<Link to="/signup" className={style.NavBarLink}>
 				SIGN UP
 			</Link>
 		</div>
 	);
 
 	return (
-		<div className="NavBar" align="center">
+		<div className="navBar" align="center">
 			{nav}
 		</div>
 	);
