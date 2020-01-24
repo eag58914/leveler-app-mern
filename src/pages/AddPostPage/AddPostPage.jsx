f73f0cdec82880b1a586696be469343e7afa8f8e
Elco Garcia <garciaelco18@gmail.com>
2020-01-24 13:38:14 -0600

    css changes

--- src/pages/AddPostPage/AddPostPage.jsx
+++ src/pages/AddPostPage/AddPostPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import PostForm from '../../components/PostForm/PostForm';
+import styles from './AddPostPage.module.css';
 
 function AddPage(props) {
-	return <PostForm handleAddPost={props.handleAddPost} />;
+	return (
+		<div className={styles.background}>
+			<h2> Create Post</h2>
+			<PostForm handleAddPost={props.handleAddPost} />
+		</div>
+	);
 }
 
 export default AddPage;
